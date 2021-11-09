6bbdc2f075cfc92b345028b724fcdd5ece7933e7
surya <surya27bk@gmail.com>
2021-11-09 15:25:42 +0700

    tugas webgis (final fix)

--- application/views/layout/viewhead.php
+++ application/views/layout/viewhead.php
@@ -12,11 +12,11 @@
 
     <!-- Font Awesome Icons -->
 
-    <link rel="stylesheet" href="<?= base_url() ?>asset/template/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/template/plugins/fontawesome-free/css/all.min.css">
 
     <!-- Theme style -->
 
-    <link rel="stylesheet" href="<?= base_url() ?>asset/template/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/template/dist/css/adminlte.min.css">
 
     <!-- Google Font: Source Sans Pro -->
 
@@ -25,15 +25,15 @@
 
     <!-- jQuery -->
 
-    <script src="<?= base_url() ?>asset/template/plugins/jquery/jquery.min.js"></script>
+    <script src="<?= base_url() ?>assets/template/plugins/jquery/jquery.min.js"></script>
 
     <!-- Bootstrap 4 -->
 
-    <script src="<?= base_url() ?>asset/template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url() ?>assets/template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- AdminLTE App -->
 
-    <script src="<?= base_url() ?>asset/template/dist/js/adminlte.min.js"></script>
+    <script src="<?= base_url() ?>assets/template/dist/js/adminlte.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 
--- application/views/layout/viewheader.php
+++ application/views/layout/viewheader.php
@@ -4,15 +4,15 @@
 
         <!-- Navbar -->
 
-        <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+        <nav class="main-header navbar navbar-expand-md navbar-dark navbar-dark">
 
             <div class="container">
 
-                <a href="<?= base_url() ?>asset/template/index3.html" class="navbar-brand">
+                <a href="<?= base_url() ?>assets/template/index3.html" class="navbar-brand">
 
-                    <img src="<?= base_url() ?>asset/template/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                    <img src="<?= base_url() ?>assets/template/dist/img/world-logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
 
-                    <span class="brand-text font-weight-light">AdminLTE 3</span> </a>
+                    <span class="brand-text font-weight-light">WEB GIS</span> </a>
 
                 <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
 
