2135e77b285026ac5232d1c7cdbf57582b62ce9c
surya <surya27bk@gmail.com>
2021-11-09 13:20:49 +0700

    tambah file

---
+++ application/controllers/Home.php
@@ -0,0 +1,24 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+    public function index()
+
+    {
+
+        $data = array(
+
+            'judul' => 'WebGIS Trainning',
+
+            'content' => 'peta_leaflet'
+
+        );
+
+        $this->load->view('layout/viewunion', $data, FALSE);
+    }
+}
+
+/* End of file Home.php */
---
+++ application/views/layout/viewhead.php
@@ -0,0 +1,53 @@
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>AdminLTE 3 | Top Navigation</title>
+
+    <!-- Font Awesome Icons -->
+
+    <link rel="stylesheet" href="<?= base_url() ?>asset/template/plugins/fontawesome-free/css/all.min.css">
+
+    <!-- Theme style -->
+
+    <link rel="stylesheet" href="<?= base_url() ?>asset/template/dist/css/adminlte.min.css">
+
+    <!-- Google Font: Source Sans Pro -->
+
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <!-- REQUIRED SCRIPTS -->
+
+    <!-- jQuery -->
+
+    <script src="<?= base_url() ?>asset/template/plugins/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap 4 -->
+
+    <script src="<?= base_url() ?>asset/template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- AdminLTE App -->
+
+    <script src="<?= base_url() ?>asset/template/dist/js/adminlte.min.js"></script>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+
+    <link rel="stylesheet" href="<?= base_url() ?>assets/leaflet.groupedlayercontrol.css" />
+    <script src="<?= base_url() ?>assets/leaflet.groupedlayercontrol.js"></script>
+
+    <link rel="stylesheet" href="<?= base_url() ?>assets/Control.MiniMap.css" />
+    <script src="<?= base_url() ?>assets/Control.MiniMap.js"></script>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
+    <script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
+
+    <link rel="stylesheet" href="https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-locatecontrol/v0.43.0/L.Control.Locate.css">
+    <script src="https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-locatecontrol/v0.43.0/L.Control.Locate.min.js"></script>
+
+</head>
